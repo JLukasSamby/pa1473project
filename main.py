2d9b38779a46a626c1370156d51984ba878d8e70
Lukas Samby <ls224kr@student.lnu.se>
2023-04-04 09:31:04 +0200

    Development: Added sensor objects and port constants

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (
+    Motor,
+    TouchSensor,
+    ColorSensor,
+    InfraredSensor,
+    UltrasonicSensor,
+    GyroSensor,
+)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -10,13 +16,13 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-
+TOUCH_SENSOR_PORT
+COLOR_SENSOR_PORT
 
 # Create your objects here.
 ev3 = EV3Brick()
-
+touchSensor = Motor(TOUCH_SENSOR_PORT)
+colorSensor = Motor(COLOR_SENSOR_PORT)
 
 # Write your program here.
 ev3.speaker.beep()
-
-# Print ("Hello World")
