b0af059fcd447c02e7f09e21a33c6d7590976309
Lukas Samby <ls224kr@student.lnu.se>
2023-04-05 09:22:55 +0200

    Development: Test program

--- main.py
+++ main.py
@@ -30,5 +30,14 @@ clawMotor = Motor(CLAW_MOTOR_PORT)
 craneMotor = Motor(CRANE_MOTOR_PORT)
 rotationMotor = Motor(ROTATION_MOTOR_PORT)
 
+craneMotor.control.limits(speed=60, acceleration=120)
+rotationMotor.control.limits(speed=60, acceleration=120)
+
+craneMotor.run_time(-30, 1000)
+craneMotor.run(15)
+while colorSensor.reflection() < 32:
+    wait(10)
+craneMotor.reset_angle(0)
+craneMotor.hold()
+
 # Write your program here.
-ev3.speaker.beep()
