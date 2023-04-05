674cbfb2f93d179f2e4a3bd640d6fb38d3d6f858
Lukas Samby <ls224kr@student.lnu.se>
2023-04-05 09:19:02 +0200

    Development: Added all sensor and motor definitions and updated constants

--- main.py
+++ main.py
@@ -16,13 +16,19 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-TOUCH_SENSOR_PORT = 0
-COLOR_SENSOR_PORT = 0
+TOUCH_SENSOR_PORT = Port.S1
+COLOR_SENSOR_PORT = Port.S2
+CLAW_MOTOR_PORT = Port.A
+CRANE_MOTOR_PORT = Port.B
+ROTATION_MOTOR_PORT = Port.C
 
 # Create your objects here.
 ev3 = EV3Brick()
-touchSensor = Motor(TOUCH_SENSOR_PORT)
-colorSensor = Motor(COLOR_SENSOR_PORT)
+touchSensor = TouchSensor(TOUCH_SENSOR_PORT)
+colorSensor = ColorSensor(COLOR_SENSOR_PORT)
+clawMotor = Motor(CLAW_MOTOR_PORT)
+craneMotor = Motor(CRANE_MOTOR_PORT)
+rotationMotor = Motor(ROTATION_MOTOR_PORT)
 
 # Write your program here.
 ev3.speaker.beep()
