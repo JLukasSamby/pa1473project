0091b242cb277cb33e90a04456fa3070d443cbea
Lukas Samby <ls224kr@student.lnu.se>
2023-04-05 11:00:37 +0200

    Refactor: Added new init and main functions and sections for constants

--- main.py
+++ main.py
@@ -18,13 +18,26 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
+
+# ----------------------------------------
+# Constants
+# ----------------------------------------
+
+# Port constants
 TOUCH_SENSOR_PORT = Port.S1
 COLOR_SENSOR_PORT = Port.S2
 CLAW_MOTOR_PORT = Port.A
 CRANE_MOTOR_PORT = Port.B
 ROTATION_MOTOR_PORT = Port.C
 
-# Create your objects here.
+# Gear constants
+BIG_GEAR_TOOTHCOUNT = 40
+SMALL_GEAR_TOOTHCOUNT = 8
+CRANE_GEAR_RATIO = BIG_GEAR_TOOTHCOUNT / SMALL_GEAR_TOOTHCOUNT
+
+# ----------------------------------------
+# Objects
+# ----------------------------------------
 ev3 = EV3Brick()
 touchSensor = TouchSensor(TOUCH_SENSOR_PORT)
 colorSensor = ColorSensor(COLOR_SENSOR_PORT)
@@ -35,20 +48,26 @@ rotationMotor = Motor(ROTATION_MOTOR_PORT)
 craneMotor.control.limits(speed=60, acceleration=120)
 rotationMotor.control.limits(speed=60, acceleration=120)
 
-#code under here.
-BIG_GEAR = 40
-SMALL_GEAR = 8
-GEAR_RATIO = BIG_GEAR/SMALL_GEAR
+# ----------------------------------------
+# Code
+# ----------------------------------------
 
 
+def init():
+    craneMotor.run_time(-60, 3000)
+    craneMotor.run(20)
 
-craneMotor.run_time(-60, 3000)
-craneMotor.run(20)
+    while colorSensor.reflection() < 15:
+        print(colorSensor.reflection())
+        wait(10)
+    craneMotor.reset_angle(12 * CRANE_GEAR_RATIO)
+    craneMotor.run_target(45, 0)
 
-while colorSensor.reflection() < 15:
-    print(colorSensor.reflection())
-    wait(10)
-craneMotor.reset_angle(12*gearRatio)
-craneMotor.run_target(45,0)
-while True:
-    craneMotor.hold()
+
+def main():
+    init()
+    while True:
+        craneMotor.hold()
+
+
+main()
