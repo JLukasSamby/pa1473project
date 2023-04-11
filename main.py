26b203e4ae4136e99441c6105314b666daffcb77
ozon <amir.ignoubourkier@gmail.com>
2023-04-11 14:19:27 +0200

    Drop,color sorting update

--- main.py
+++ main.py
@@ -15,13 +15,19 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 # ----------------------------------------
 # Constants
 # ----------------------------------------
+COLOR_ZERO = 10
+
+# Speed constant
+LOW_SPEED = 30
+MEDIUM_SPEED = 45
+HIGH_SPEED = 60
+VERY_HIGH_SPEED = 80
 
 # Port constants
 TOUCH_SENSOR_PORT = Port.S1
@@ -31,12 +37,29 @@ CRANE_MOTOR_PORT = Port.B
 ROTATION_MOTOR_PORT = Port.C
 
 # Gear constants
-BIG_GEAR_TOOTHCOUNT = 40
-SMALL_GEAR_TOOTHCOUNT = 8
-CRANE_GEAR_RATIO = BIG_GEAR_TOOTHCOUNT / SMALL_GEAR_TOOTHCOUNT
+CRANE_BIG_GEAR_TOOTHCOUNT = 40
+CRANE_SMALL_GEAR_TOOTHCOUNT = 8
+CRANE_GEAR_RATIO = CRANE_BIG_GEAR_TOOTHCOUNT / CRANE_SMALL_GEAR_TOOTHCOUNT
+
+ROTATION_BIG_GEAR_TOOTHCOUNT = 36
+ROTATION_SMALL_GEAR_TOOTHCOUNT = 12
+ROTATION_GEAR_RATIO = ROTATION_BIG_GEAR_TOOTHCOUNT / ROTATION_SMALL_GEAR_TOOTHCOUNT
 
 # Angle constants
 COLOR_TO_FLOOR_ANGLE = 30 * CRANE_GEAR_RATIO
+
+# Color constants
+BASE_ANGLE = 190
+COLOR_DICTIONARY = {
+    Color.BLACK: 0,
+    Color.BLUE:     BASE_ANGLE * 1 / 6,
+    Color.GREEN:    BASE_ANGLE * 2 / 6,
+    Color.YELLOW:   BASE_ANGLE * 3 / 6,
+    Color.RED:      BASE_ANGLE * 4 / 6, ##test
+    Color.WHITE:    BASE_ANGLE * 5 / 6,
+    Color.BROWN:    BASE_ANGLE * 6 / 6,
+    None:          -BASE_ANGLE * 1 / 6,
+}
 
 # ----------------------------------------
 # Objects
@@ -49,8 +72,8 @@ clawMotor = Motor(CLAW_MOTOR_PORT)
 craneMotor = Motor(CRANE_MOTOR_PORT)
 rotationMotor = Motor(ROTATION_MOTOR_PORT)
 
-craneMotor.control.limits(speed=60, acceleration=120)
-rotationMotor.control.limits(speed=60, acceleration=120)
+craneMotor.control.limits(speed=VERY_HIGH_SPEED, acceleration=120)
+rotationMotor.control.limits(speed=VERY_HIGH_SPEED, acceleration=120)
 
 # ----------------------------------------
 # Code
@@ -58,43 +81,71 @@ rotationMotor.control.limits(speed=60, acceleration=120)
 
 
 def init():
-    clawMotor.run_until_stalled(-60, then=Stop.COAST, duty_limit=25)
+    # Initialize claw motor
+    clawMotor.run_until_stalled(-HIGH_SPEED, then=Stop.COAST, duty_limit=25)
 
     clawMotor.reset_angle(0)
-    clawMotor.run_target(60, 55)
+    clawMotor.run_target(HIGH_SPEED, 55)
 
-    craneMotor.run_time(-60, 3500)
-    craneMotor.run(20)
+    # Initialize crane motor
+    craneMotor.run_time(-HIGH_SPEED, 3500)
+    craneMotor.run(LOW_SPEED)
 
-    while colorSensor.reflection() < 15:
+    while colorSensor.reflection() < COLOR_ZERO:
         print(colorSensor.reflection())
-        wait(10)
+        wait(5)
     craneMotor.reset_angle(12 * CRANE_GEAR_RATIO)
-    craneMotor.run_target(45, 0)
+    craneMotor.run_target(MEDIUM_SPEED, 0)
+
+    # Initialize rotation motor
+    rotationMotor.reset_angle(0)
 
 
-def grip_item():
-    pass
-
-
-def pick_up():
+def pick_item():
     initialAngle = craneMotor.angle()
-    clawMotor.run_target(60, 55, wait=False)
-    craneMotor.run_until_stalled(60, then=Stop.COAST, duty_limit=100 / CRANE_GEAR_RATIO)
-    clawMotor.run_until_stalled(-60, then=Stop.HOLD, duty_limit=50)
-    craneMotor.run_target(30, initialAngle)
+    clawMotor.run_target(HIGH_SPEED, 55, wait=False)
+    craneMotor.run_until_stalled(HIGH_SPEED, then=Stop.COAST, duty_limit=100 / CRANE_GEAR_RATIO)
+    clawMotor.run_until_stalled(-HIGH_SPEED, then=Stop.HOLD, duty_limit=50)
+    craneMotor.run_target(LOW_SPEED, initialAngle)
 
 
 def drop_item():
     initialAngle = craneMotor.angle()
-    craneMotor.run_until_stalled(60, then=Stop.COAST, duty_limit=100 / CRANE_GEAR_RATIO)
-    clawMotor.run_target(60)
-    craneMotor.run_target(30, initialAngle)
+    craneMotor.run_until_stalled(HIGH_SPEED, then=Stop.COAST, duty_limit=100 / CRANE_GEAR_RATIO)
+    clawMotor.run_target(HIGH_SPEED, 55)
+    craneMotor.run_target(LOW_SPEED, initialAngle)
+
+
+def get_color():
+    return colorSensor.color()
+
+
+# REfactor to use a "do_at" function taking a function and an angle. To remove duplicated code
+
+def drop_item_at(angle):
+    initialAngle = rotationMotor.angle()
+    rotationMotor.run_target(HIGH_SPEED, -ROTATION_GEAR_RATIO * angle)
+    drop_item()
+    rotationMotor.run_target(HIGH_SPEED, ROTATION_GEAR_RATIO * initialAngle)
+
+
+def pick_item_at(angle):
+    initialAngle = rotationMotor.angle()
+    rotationMotor.run_target(HIGH_SPEED, ROTATION_GEAR_RATIO * angle)
+    drop_item()
+    rotationMotor.run_target(HIGH_SPEED, ROTATION_GEAR_RATIO * initialAngle)
+
+
+def drop_item_by_color():
+    pick_item()
+    color = get_color()
+    angle = COLOR_DICTIONARY[color]
+    drop_item_at(angle)
 
 
 def main():
     init()
-    pick_up()
+    drop_item_by_color()
     while True:
         craneMotor.hold()
 
