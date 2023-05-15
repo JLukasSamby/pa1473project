74505fcaca841957a4cbe6d5cb32199132345cd8
lil weeb <46575772+DanyaKreyg@users.noreply.github.com>
2023-05-15 21:15:30 +0200

    Update sorter_constants.py
    Tiene un gusto exquisito

--- sorter_constants.py
+++ sorter_constants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-
+""" A collection of constants that sorter_client.py amd sorter_server.py use """
 MBOX_NAME = "PA1473_GROUP8_MAILBOX"
 SERVER_NAME = 'ev3dev'
 EXIT_MESSAGE = "EXIT"
