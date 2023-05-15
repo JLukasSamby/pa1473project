ae0f8b997e7098dad87d100bb67228ee84ccd276
JLukasSamby <97880345+JLukasSamby@users.noreply.github.com>
2023-05-15 21:15:21 +0200

    Update pick.py

--- pick.py
+++ pick.py
@@ -16,15 +16,21 @@ from pybricks.parameters import Stop
 
 
 def _open_claw() -> None:
-    """Open claw."""
+    """
+    Open claw.
+    """
     clawMotor.run_target(HIGH_SPEED, CLAW_OPEN_ANGLE)
 
 def _close_claw() -> None:
-    """Close claw."""
+    """
+    Close claw.
+    """
     clawMotor.run_until_stalled(-HIGH_SPEED, then=Stop.HOLD, duty_limit=MAX_DUTY * 0.8)
 
 def _pick_stalled() -> None:
-    """Drop item using run_until_stalled."""
+    """
+    Drop item using run_until_stalled.
+    """
     _open_claw()
     craneMotor.run_until_stalled(
         HIGH_SPEED, then=Stop.COAST, duty_limit=MAX_DUTY / CRANE_GEAR_RATIO
@@ -33,7 +39,12 @@ def _pick_stalled() -> None:
     craneMotor.run_target(HIGH_SPEED, 0)
 
 def _pick_height(height: float) -> None:
-    """Drop item using preconfigured height."""
+    """
+    Drop item using preconfigured height.
+    
+    Parameters:
+        height (float): height to pick from given in angles of crane motor rotation.
+    """
     _open_claw()
     craneMotor.run_target(HIGH_SPEED, height)
     _close_claw()
