7384478e13b8b84d75dd17a06824f51056a260f1
JLukasSamby <97880345+JLukasSamby@users.noreply.github.com>
2023-05-15 20:52:57 +0200

    Update drop.py with modified dcostrings

--- drop.py
+++ drop.py
@@ -13,11 +13,15 @@ from robot_setup import (
 
 
 def _drop() -> None:
-    """Drop item."""
+    """
+    Open claw to drop currently held item.
+    """
     clawMotor.run_target(HIGH_SPEED, CLAW_OPEN_ANGLE)
 
 def drop_stalled() -> None:
-    """Drop item using run_until_stalled."""
+    """
+    Drop currently held item by running into the floor (stalling).
+    """
     craneMotor.run_until_stalled(
         HIGH_SPEED, then=Stop.COAST, duty_limit=MAX_DUTY / CRANE_GEAR_RATIO
     )
@@ -26,7 +30,12 @@ def drop_stalled() -> None:
 
 
 def drop_height(height: float) -> None:
-    """Drop item using preconfigured height."""
+    """
+    Drop item using preconfigured height.
+    
+    Parameters:
+        height (float): hegiht to drop at, given in angles of crane motor rotation.
+    """
     craneMotor.run_target(HIGH_SPEED, height)
     _drop()
     craneMotor.run_target(HIGH_SPEED, 0)
